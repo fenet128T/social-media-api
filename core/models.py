687a83abb6c04cfb6419d7ae9b396bd1977a2ccc
fenet128t <fenet128t@gmail.com>
2026-02-12 23:33:24 +0300

    add post model and follow model with proper constraints and clear relationships

--- core/models.py
+++ core/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
+from django.db.models import Q
+from django.core.exceptions import ValidationError
 
 
 class CustomUser(AbstractUser):
@@ -8,3 +11,55 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+# Post model to represent user posts in the social media application
+class Post(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    content = models.TextField()
+    media = models.ImageField(upload_to='post_media/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']  # Newest first
+
+    def __str__(self):
+        return f"{self.author.username} - {self.content[:30]}"
+    
+
+# Like model to track which users liked which posts
+class Follow(models.Model):
+    follower = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='following'
+    )
+    following = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='followers'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['follower', 'following'],
+                name='unique_follow_relationship'
+            ),
+        ]
+
+    def clean(self):
+        if self.follower == self.following:
+            raise ValidationError("You cannot follow yourself.")
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.follower.username} follows {self.following.username}"
