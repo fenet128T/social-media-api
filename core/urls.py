5a0a81b57c6f0520e465780b229e221753b7c23c
fenet128t <fenet128t@gmail.com>
2026-02-16 22:55:01 +0300

    add account setting to manage accounts

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, UserViewSet
+from .views import PostViewSet, UserViewSet,FollowViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'posts', PostViewSet)
+router.register(r'follows', FollowViewSet)
 
 urlpatterns = router.urls
